859c907b58ed58d02ebfeca180ecca9df3373d81
Chas <chasmo123@gmail.com>
2026-04-04 19:44:55 -0700

    some updates

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,47 +3,54 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+    // Camera screensize
+    public Vector2 ScreenSize { get; set; }
+    // Speed attributes
+    [Export]
+    public int Speed { get; set; } = 400;
+    // Player gameplay attributes
+    public int Lives { get; set; } = 3;
+    public int Armor { get; set; } = 0;
+    public int Level { get; set; } = 0;
+    public int Ammo { get; set; } = 10;
 
-	[Export]
-	public int Speed { get; set; } = 400;
-	public Vector2 ScreenSize { get; set; }
+    // Methods
+    public override void _Ready()
+    {
+        ScreenSize = GetViewportRect().Size;
+    }
 
-	public override void _Ready()
-	{
-		ScreenSize = GetViewportRect().Size;
-	}
+    public override void _Process(double delta)
+    {
+        Vector2 velocity = Vector2.Zero;
 
-	public override void _Process(double delta)
-	{
-		Vector2 velocity = Vector2.Zero;
+        if (Input.IsActionPressed("move_left"))
+        {
+            velocity.X -= 1;
+        }
+        if (Input.IsActionPressed("move_right"))
+        {
+            velocity.X += 1;
+        }
+        if (Input.IsActionPressed("move_up"))
+        {
+            velocity.Y -= 1;
+        }
+        if (Input.IsActionPressed("move_down"))
+        {
+            velocity.Y += 1;
+        }
 
-		if (Input.IsActionPressed("move_left"))
-		{
-			velocity.X -= 1;
-		}
-		if (Input.IsActionPressed("move_right"))
-		{
-			velocity.X += 1;
-		}
-		if (Input.IsActionPressed("move_up"))
-		{
-			velocity.Y -= 1;
-		}
-		if (Input.IsActionPressed("move_down"))
-		{
-			velocity.Y += 1;
-		}
-		
-		if (velocity.Length() > 0)
-		{
-			velocity = velocity.Normalized() * Speed;
-		}
-		
-		Position += velocity * (float)delta;
-		Position = new Vector2(
-			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
-			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
-		);
-	}
+        if (velocity.Length() > 0)
+        {
+            velocity = velocity.Normalized() * Speed;
+        }
+
+        Position += velocity * (float)delta;
+        Position = new Vector2(
+            x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+            y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+        );
+    }
 
 }
