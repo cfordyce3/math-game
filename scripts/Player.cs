e8b6913c4bb5e2778f170049aa3c3627b51d8b1f
Chas <chasmo123@gmail.com>
2026-04-04 18:57:44 -0700

    c# test for player

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,38 +3,43 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+    public const float Speed = 300.0f;
+    public const float JumpVelocity = -400.0f;
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector2 velocity = Velocity;
+    public override void _Ready()
+    {
+        // Vector2 ScreenSize = GetViewportRect().Size;
+        // Hide();
+    }
 
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}
+    public override void _Process(double delta)
+    {
+        Vector2 velocity = Velocity;
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
+        Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+        if (direction != Vector2.Zero)
+        {
+            velocity.X = direction.X * Speed;
+            velocity.Y = direction.Y * Speed;
+        }
+    }
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
+    public override void _PhysicsProcess(double delta)
+    {
+        // Vector2 veloity = Velocity;
 
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+        // Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        // if (direction != Vector2.Zero)
+        // {
+        //     velocity.X = direction.X * Speed;
+        // }
+        // else
+        // {
+        //     velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+        // }
+
+        // Velocity = velocity;
+        // MoveAndSlide();
+    }
+
 }
