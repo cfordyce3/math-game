05b15ab4643c8c208a778c6f77eca1e0b212150c
rednotorange <currenhamlet@gmail.com>
2026-04-05 23:23:19 -0400

    Added animations

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -37,24 +37,32 @@ public partial class Player : CharacterBody2D
 		{
 			velocity.X -= 1;
 			_playerSprite.FlipH = true;
+			_playerSprite.Play("idle");
 		}
 		if (Input.IsActionPressed("move_right"))
 		{
 			velocity.X += 1;
 			_playerSprite.FlipH = false;
+			_playerSprite.Play("idle");
 		}
 		if (Input.IsActionPressed("move_up"))
 		{
 			velocity.Y -= 1;
+			_playerSprite.Play("going_up");
 		}
 		if (Input.IsActionPressed("move_down"))
 		{
 			velocity.Y += 1;
+			_playerSprite.Play("idle");
 		}
 
 		if (velocity.Length() > 0)
 		{
 			velocity = velocity.Normalized() * Speed;
+		}
+		else 
+		{
+			_playerSprite.Play("idle");
 		}
 
 		Position += velocity * (float)delta;
