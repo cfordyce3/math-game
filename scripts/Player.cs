ba74fcc54aff3959358d6d40af338f370b2c2341
Chas <chasmo123@gmail.com>
2026-04-05 18:31:26 -0700

    camera

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,54 +3,58 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	// Camera screensize
-	public Vector2 ScreenSize { get; set; }
-	// Speed attributes
-	[Export] // <- shows up in the inspector sidebar
-	public int Speed { get; set; } = 400;
-	// Player gameplay attributes
-	public int Lives { get; set; } = 3;
-	public int Armor { get; set; } = 0;
-	public int Level { get; set; } = 0;
-	public int Ammo { get; set; } = 10;
+    // Camera screensize
+    public Vector2 ScreenSize { get; set; }
+    // Speed attributes
 
-	// Methods
-	public override void _Ready()
-	{
-		ScreenSize = GetViewportRect().Size;
-	}
+    [ExportGroup("Attributes")]
+    [Export] public int Speed { get; set; } = 400;
+    // Player gameplay attributes
+    [Export] public int Lives { get; set; } = 3;
+    [Export] public int Armor { get; set; } = 0;
+    [Export] public int Level { get; set; } = 0;
+    [Export] public int Ammo { get; set; } = 10;
 
-	public override void _Process(double delta)
-	{
-		Vector2 velocity = Vector2.Zero;
+    [ExportGroup("References")]
+    [Export] private CollisionShape2D _collisionBox;
 
-		if (Input.IsActionPressed("move_left"))
-		{
-			velocity.X -= 1;
-		}
-		if (Input.IsActionPressed("move_right"))
-		{
-			velocity.X += 1;
-		}
-		if (Input.IsActionPressed("move_up"))
-		{
-			velocity.Y -= 1;
-		}
-		if (Input.IsActionPressed("move_down"))
-		{
-			velocity.Y += 1;
-		}
+    // Methods
+    public override void _Ready()
+    {
+        ScreenSize = GetViewportRect().Size;
+    }
 
-		if (velocity.Length() > 0)
-		{
-			velocity = velocity.Normalized() * Speed;
-		}
+    public override void _Process(double delta)
+    {
+        Vector2 velocity = Vector2.Zero;
 
-		Position += velocity * (float)delta;
-		Position = new Vector2(
-			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
-			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
-		);
-	}
+        if (Input.IsActionPressed("move_left"))
+        {
+            velocity.X -= 1;
+        }
+        if (Input.IsActionPressed("move_right"))
+        {
+            velocity.X += 1;
+        }
+        if (Input.IsActionPressed("move_up"))
+        {
+            velocity.Y -= 1;
+        }
+        if (Input.IsActionPressed("move_down"))
+        {
+            velocity.Y += 1;
+        }
+
+        if (velocity.Length() > 0)
+        {
+            velocity = velocity.Normalized() * Speed;
+        }
+
+        Position += velocity * (float)delta;
+        Position = new Vector2(
+            x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+            y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+        );
+    }
 
 }
