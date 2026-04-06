337021f5daabb01dd72e0b1b68f9809830b1eddc
rednotorange <currenhamlet@gmail.com>
2026-04-06 14:04:48 -0400

    added animations for all directions to knight

--- scripts/HeartPickup.cs
+++ scripts/HeartPickup.cs
@@ -3,18 +3,18 @@ using System;
 
 public partial class HeartPickup : Area2D
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
 
-    public void OnBodyEntered(PhysicsBody2D body)
-    {
+	public void OnBodyEntered(PhysicsBody2D body)
+	{
 
-    }
+	}
 }
