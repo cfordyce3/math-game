f0dff41cba585e345f1aa4fff7179c106ae9bc25
Chas <chasmo123@gmail.com>
2026-04-19 23:30:39 -0700

    Removed redundant text from HUD

--- scripts/Hud.cs
+++ scripts/Hud.cs
@@ -34,8 +34,8 @@ public partial class Hud : CanvasLayer
 		_weapon = (_player.GetPlayerWeapon() == 0) ? "sword" : "bow";
 		_readyToShoot = _player._readyToShoot;
 
-		_statsCounter.Text = String.Format("Lives: {0}\nLevel: {1}\nArrows Left: {2}\nArmor Level:{3}\n\nWeapon: {4}", _lives, _level,
-			_ammo, _armor, _weapon);
+		_statsCounter.Text = String.Format("Level: {0}\nArmor Level:{1}\n\nWeapon: {2}", _level,
+			_armor, _weapon);
 		_statsCounter.Text += String.Format("\nReady to shoot: {0}", _readyToShoot);
 	}
 
