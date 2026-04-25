efeccc3a5aaa8d07cde819c5c78488f53d142d4a
Chas <chasmo123@gmail.com>
2026-04-24 19:45:38 -0700

    some more work on animations

--- scripts/player/PlayerAnimations.cs
+++ scripts/player/PlayerAnimations.cs
@@ -1,14 +1,34 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class PlayerAnimations : AnimatedSprite2D
 {
 	private Player _player;
 	private ShapeCast2D _swordSwingHitbox;
 	
+	private Node _sceneTree;
+	
+	private List<ulong> _enemiesList = [];
+	
 	[Signal] public delegate void GetAnimationDetailsEventHandler(string animationName, int frameIndex);
 	[Signal] public delegate void AnimationFinishedWithNameEventHandler(string animationName);
 	[Signal] public delegate void BowReleasedEventHandler();
+
+	public override void _EnterTree()
+	{
+		_sceneTree = GetTree().Root.GetNode("Game");
+		var nodeCount = _sceneTree.GetChildren();
+		// find every orc or skeleton and add it to the enemies list
+		foreach (var node in  nodeCount)
+		{
+			if (node.Name.ToString().Contains("Skeleton") || node.Name.ToString().Contains("Orc"))
+			{
+				_enemiesList.Add(node.GetInstanceId());
+			}
+		}
+	}
+
 	public override void _Ready()
 	{
 		_player = GetParent<Player>();
@@ -27,6 +47,43 @@ public partial class PlayerAnimations : AnimatedSprite2D
 		EmitSignal(SignalName.AnimationFinishedWithName, Animation);
 	}
 
+	public void HandleSwordSwing()
+	{
+		// Track whether player is flipped or not
+		bool flipped = _player.GetPlayerFlipped();
+			
+		// Sets root (rotation point) position
+		_swordSwingHitbox.Position = (flipped) ? new Vector2(-3, 1) : new Vector2(3, 1);
+			
+		// Set rotation based on animation frame
+		switch (Frame)
+		{
+			case 0:
+			case 1:
+				break;
+			case 2: // frame 2
+				_swordSwingHitbox.Enabled = true; // enables hitbox on first frame
+				_swordSwingHitbox.RotationDegrees = (flipped) ? 55 : -60;
+				break;
+			case 3: // frame 3
+				_swordSwingHitbox.RotationDegrees = (flipped) ? 130 : -130;
+				break;
+			case 4: // frame 4
+				_swordSwingHitbox.RotationDegrees = (flipped) ? 155 : -160;
+				break;
+		}
+
+		if (_swordSwingHitbox.IsColliding())
+		{
+			var collidedWith = _swordSwingHitbox.GetCollider(0).GetInstanceId();
+			
+			if (collidedWith != null && _enemiesList.Contains(collidedWith))
+			{
+				GD.Print("collided with " + collidedWith);
+			}
+		}
+	}
+
 	public override void _Process(double delta)
 	{
 		// Send out animation details every frame
@@ -35,28 +92,9 @@ public partial class PlayerAnimations : AnimatedSprite2D
 		// When bow is ready to release
 		if (Animation == "bow_attack" && Frame == 3) EmitSignal(SignalName.BowReleased);
 		
-		if (Animation == "sword_attack")
-		{
-			bool flipped = _player.GetPlayerFlipped();
-			_swordSwingHitbox.Position = (flipped) ? new Vector2(-3, 1) : new Vector2(3, 1); // sets base position
-			switch (Frame)
-			{
-				case 0:
-				case 1:
-					break;
-				case 2: // frame 2
-					_swordSwingHitbox.Enabled = true; // enables hitbox on first frame
-					_swordSwingHitbox.RotationDegrees = (flipped) ? 55 : -60;
-					break;
-				case 3: // frame 3
-					_swordSwingHitbox.RotationDegrees = (flipped) ? 130 : -130;
-					break;
-				case 4: // framr 4
-					_swordSwingHitbox.RotationDegrees = (flipped) ? 155 : -160;
-					break;
-			}
-		}
-		else _swordSwingHitbox.Enabled = false; // if not attacking, no hitbox
+		// When swinging sword
+		if (Animation == "sword_attack") HandleSwordSwing();
+		else _swordSwingHitbox.Enabled = false; // if not attacking, no sword hitbox
 
 	}
 }
